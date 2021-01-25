b95f69088f8fa3a8961953b9797a1b2fe09d54f6
bipularyal <bipularyal01@gmail.com>
2021-01-25 23:36:44 +0545

    Updates

---
+++ precache-manifest.a0804bbe06bce835204ea89c927c64f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "493a2a063b1d9bc6572d253e62f1304d",
+    "url": "/react-monster-rolodex/index.html"
+  },
+  {
+    "revision": "4d650dde0b00a46f87ea",
+    "url": "/react-monster-rolodex/static/css/main.c7d8ebd2.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/react-monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d650dde0b00a46f87ea",
+    "url": "/react-monster-rolodex/static/js/main.001244cf.chunk.js"
+  },
+  {
+    "revision": "e5eb423b2a2cc1d9f46f",
+    "url": "/react-monster-rolodex/static/js/runtime-main.856af338.js"
+  }
+]);
